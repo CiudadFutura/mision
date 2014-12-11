00cddb5a8d91614c7f6ddd82dac174a31452e6c5
elunca <nicofheredia@gmail.com>
2014-12-11 18:59:17 -0300

    Home con fonts y less

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,12 @@ class HomeController < ApplicationController
 
     user_type = current_usuario.nil? ? 'Guess' : current_usuario.type
     case user_type
-    when Usuario::ADMIN
-      render 'home_coord'
-    when Usuario::COORDINADOR
-      render 'home_coord'
-    when Usuario::USUARIO
-      redirect_to productos_path
-    else
-      redirect_to productos_path
+      when Usuario::ADMIN
+        render 'home_coord'
+      when Usuario::COORDINADOR
+        render 'home_coord'
+      when Usuario::USUARIO
+        redirect_to productos_path
     end
   end
 end
