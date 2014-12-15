03cb758048f65dc92b0b94cc6f1b92eec6a6e479
Andres Cachero <acachero-c@g2llc.com.ar>
2014-12-15 12:02:48 -0300

    Agregar templates de gema Devise. Dar formato a la pagina de registracion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126180308) do
+ActiveRecord::Schema.define(version: 20141215142735) do
 
   create_table "categorias", force: true do |t|
     t.string   "nombre"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20141126180308) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.integer  "circulo_id"
+    t.string   "dni"
   end
 
   add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true
