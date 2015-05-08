bd75f237c36a9365a055664b0d926946536f0930
Andres Cachero <acachero-c@g2llc.com.ar>
2015-05-08 16:47:17 -0300

    Disable user to make a pedido if it doesn't belong to a circulo

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -2,4 +2,6 @@ class Pedido < ActiveRecord::Base
   belongs_to :circulo
   belongs_to :usuario
   belongs_to :ciclo, class_name: "Compra", foreign_key: :compra_id
+
+  validate :circulo, presence: true
 end
