1a4f4bb424c34cddfe481f3a5e3a1fc326b9737f
Andres Cachero <acachero-c@g2llc.com.ar>
2015-05-21 13:10:10 -0300

    Prepare image to be seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,43 +13,36 @@ categorias.each do |c|
   Categoria.create!(nombre: c)
 end
 
+proveedor = Supplier.create!(name: 'Generico', nature: :wholesaler)
+categoria_id = 1
+categoria = Categoria.find_by_id(categoria_id)
+productos = []
+
 JSON.parse(open("db/products.json").read).each do |p|
   JSON.parse(open("db/products_description.json").read).each do |pd|
-    if p.products_id == pd.products_id
-      productos =[
-          { categoria_id: 1, nombre:pd.products_name , descripcion: pd.products_description,  precio: p.products_price, precio_super:0}
-      ]
+    if p['products_id'] == pd['products_id']
+      
+      p = { codigo: p['products_id'],
+            nombre: pd['products_name'],
+            descripcion: pd['products_description'],
+            precio: p['products_price'], 
+            precio_super: 0,
+          }
+          
+      if Rails.env.production?
+        img_dir = '/home/deploy/mision/shared/tmp/imagenes/'
+        image = File.open("#{img_dir}img_no_disponible.png")
+        image_path = "#{img_dir}#{p['products_image']}"
+        image = File.open(image_path) if File.owned?(image_path)
+        p[:imagen] = image
+      end
+
+      prod = Producto.new(p)
+      prod.categorias << categoria
+      prod.supplier = proveedor
+      prod.save!
     end
   end
-end
-=begin
-productos = [
-  { categoria_id: 1, nombre: 'Canasta Frutas y Verduras Chica', descripcion: 'Tomate 1 kg; Zanahoria 1/2 kg; Papa 1 kg; Cebolla 1 kg; Pimiento 1/2 kg; Banana 1 kg; Calabaza 1; Lechuga Paquete; Acelga  Bandeja 1kg; Naranja  1 kg; Huevo Media docena.',  precio: '70', precio_super: '74' },
-  { categoria_id: 1, nombre: 'Canasta frutas y verduras Grande', descripcion: '  Tomate 1 kg; Zanahoria 1/2 kg; Papa 1 kg; Batata 1 kg; Cebolla 1 kg; Pimiento 1/2 kg; Mandarina 1kg; Limon 1/2 kg; Ajo 1; Manzana 1 kg; Banana 1 kg; Calabaza 1; Lechuga Paquete; Cebolla de verdeo Paquete; Puerro Paquete; Rucula Paquete; Repollo Paquete; Brocoli Bandeja; Coliflor Bandeja; Espinaca Paquete; Radicheta Paquete; Remolacha Paquete; Acelga  Bandeja 1kg; Naranja  1 kg; Huevo Media docena.',  precio: 150, precio_super: 150 },
-  { categoria_id: 4, nombre: 'Ensaladas Mixta (Radicheta y zanahoria)', descripcion: 'Naturalísima Bandeja 300 g',  precio: 8.4, precio_super: 9 },
-  { categoria_id: 4, nombre: 'Bandeja espinaca  Naturalísima', descripcion: 'Bandeja 500 g.', precio: 6.3, precio_super: 7 },
-  { categoria_id: 4, nombre: 'Verduras para sopa SOPITAS  Naturalísima', descripcion: ' Bandeja 500 g.', precio: 15,    precio_super: 17 },
-  { categoria_id: 4, nombre: 'Rúcula hojas  Naturalísima', descripcion: ' Bandeja 200 g.', precio: 6.3, precio_super: 7 },
-  { categoria_id: 4, nombre: 'Zanahoria rallada Naturalísima', descripcion: ' Bandeja 350grs 14.7',    precio: 10, precio_super: 20 },
-  { categoria_id: 4, nombre: 'Ensaladas de Zanahoria, lechuga, cherri Naturalísima', descripcion: ' Bandeja 350grs', precio: 14.7, precio_super: 15 },
-  { categoria_id: 2, nombre: 'Huevos 100% Orgánicos', descripcion: ' Gallinas libres 1/2 docena', precio: 10,    precio_super: 18 },
-  { categoria_id: 3, nombre: 'Canasta Frescos',  descripcion: '2 Paquetes Tapas de empanada Mil hojas  x 12, 2 Paquetes Tapas de tarta Mil hojas ; Ravioles Mil hojas 1 kg; Fideos Frescos 1/2 kg ; Muzzarela La Resistencia 500g; Manteca 200gr La Cabaña/Inty ; Crema 360gr La Cabaña/Inty .', precio: 133, precio_super: 150 },
-  { categoria_id: 3, nombre: 'Reggianito', descripcion: '  La Resistencia 250g.', precio: 25, precio_super: 29 },
-  { categoria_id: 3, nombre: 'Quesos Saborizados La Resistencia', descripcion: ' Pimienta ahumada 350gr', precio: 35.7, precio_super: 34 },
-  { categoria_id: 5, nombre: 'Shampoo Prunelle 930 cc', descripcion: '', precio: 10.2, precio_super: 18 },
-  { categoria_id: 5, nombre: 'Acondicionador  Prunelle 930 cc', descripcion: '', precio: 10.2, precio_super: 17 },
-  { categoria_id: 5, nombre: 'Shampoo Tresemmé 400 ml', descripcion: '', precio: 29.7, precio_super: 35 },
-  { categoria_id: 6, nombre: 'Jabón líquido Ariel 800 cc', descripcion: '', precio: 25.2, precio_super: 27 },
-  { categoria_id: 6, nombre: 'Jabón líquido Libertador 700 cc', descripcion: '', precio: 15.3, precio_super: 17 },
-  { categoria_id: 6, nombre: 'Jabón en polvo  Drive 800gr', descripcion: '', precio: 13.2, precio_super: 16 }
-]
-=end
-
-productos.each do |p|
-  categoria_id = p[:categoria_id]
-  prod = Producto.create!(p.with_indifferent_access.except(:categoria_id))
-  prod.categorias << Categoria.find_by_id(categoria_id)
-  prod.save!
 end
 
 usuarios = [
