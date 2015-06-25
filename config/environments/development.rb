97908ea10cc1c575487c559633eef1f321134c7d
Andres Cachero <acachero-c@g2llc.com.ar>
2015-06-25 15:25:40 -0300

    Disable development env to send emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,14 +35,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "ftbn-wbbl.accessdomain.com",
-   :port                 => 587,
-   :user_name            => 'victoriacolectiva@misionantiinflacion.com.ar',
-   :password             => '!QAZzaq1',
-   :authentication       => "plain",
-  :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :test
 end
