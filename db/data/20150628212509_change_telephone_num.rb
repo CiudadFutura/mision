cc90f888c7be69fc040119dbb2d569db7ebaca1a
Andres Cachero <acachero-c@g2llc.com.ar>
2015-06-28 18:38:09 -0300

    Validate user

--- db/data/20150628212509_change_telephone_num.rb
+++ db/data/20150628212509_change_telephone_num.rb
@@ -3,6 +3,7 @@ class ChangeTelephoneNum < SeedMigration::Migration
     if Rails.env.production?
       JSON.parse(open("db/json/usuarios_purgados.json").read).each do |u|
         user = Usuario.where(email: u['email']).first
+        next if user.nil?
         user.tel1 = u['tel1']
         user.cel1 = u['cel1'] 
         user.save!
