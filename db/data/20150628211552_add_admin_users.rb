c7efade3dd9446ca37d0b5c1f109c693cfaa3a9a
Andres Cachero <acachero-c@g2llc.com.ar>
2015-06-28 18:24:01 -0300

    Add Admin user

---
+++ db/data/20150628211552_add_admin_users.rb
@@ -0,0 +1,21 @@
+class AddAdminUsers < SeedMigration::Migration
+  def up
+    if Rails.env.production?
+
+      usuario = { 
+        nombre: 'Administrador',
+        apellido: '-', 
+        password: 'Admin!QAZzaq1', 
+        email: 'victoriacolectiva@misionantiinflacion.com.ar', 
+        type: 'Admin' }
+
+      user = Usuario.new(u)
+      user.save!(validate: false)
+
+    end
+  end
+
+  def down
+
+  end
+end
