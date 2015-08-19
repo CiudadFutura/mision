a8dd7dd63ba86e6e75c57a1dd88e69ed030b2991
Andres Cachero <acachero-c@g2llc.com.ar>
2015-08-19 16:26:50 -0300

    Eliminar admin como tipo de usuario valido del form

--- spec/features/sign_in_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe "the signin process", :type => :feature do
-
-  it 'is true' do
-    visit('/usuarios/sign_up')
-    page.has_content?('Consumidor Colaborativo')
-  end
-end
