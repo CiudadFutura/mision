fdb1874694f47127c203c569092ac526b57d9bee
Nicolas Heredia <nicofheredia@gmail.com>
2015-12-27 18:39:35 -0300

    Get only data completed from DB

--- app/controllers/geo_report_controller.rb
+++ app/controllers/geo_report_controller.rb
@@ -3,6 +3,6 @@ class GeoReportController < ApplicationController
   # GET /usuarios
   # GET /usuarios.json
   def index
-    @usuarios = Usuario.where(['type != "Admin"'])
+    @usuarios = Usuario.where(['type <> "Admin" AND calle IS NOT NULL AND calle <> "" '])
   end
 end
