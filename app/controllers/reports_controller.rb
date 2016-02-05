b525c46ba91d2c7786b6cc3926fd84cb5cad9740
Nicolas Heredia <nicofheredia@gmail.com>
2016-02-05 17:33:30 -0300

    Added Ciclos  select drop down in the index page for to filter action

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,12 +4,14 @@ class ReportsController < ApplicationController
   # GET /reports.json
   def index
     if current_usuario.admin?
-        @ciclos = Compra.all.order('fecha_fin_compras DESC')
-        ciclo_id = params[:ciclo_id] || Compra.ciclo_actual.id
+      @ciclos = Compra.all.order('fecha_fin_compras DESC')
+      if params[:ciclo_id].present? || Compra.ciclo_actual_completo.present?
+        ciclo_id = params[:ciclo_id] || Compra.ciclo_actual_completo.id
         p ciclo_id
         p "-----------------------"
         pedidos = Pedido.where(compra_id: ciclo_id)
         @reporte = Reporte.pedidos_por_proveedor(pedidos)
+      end
     end
   end
 
