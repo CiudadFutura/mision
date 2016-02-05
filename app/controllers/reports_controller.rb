d6792805a4f81dacff8578d80c5852a7a84a1bfb
Nicolas Heredia <nicofheredia@gmail.com>
2016-02-05 17:08:39 -0300

    Added Ciclos  select drop down in the index page for to filter action

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,6 +4,7 @@ class ReportsController < ApplicationController
   # GET /reports.json
   def index
     if current_usuario.admin?
+        @ciclos = Compra.all.order('fecha_fin_compras DESC')
         ciclo_id = params[:ciclo_id] || Compra.ciclo_actual.id
         p ciclo_id
         p "-----------------------"
