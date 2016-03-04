37d0d7fc8e3a823e4b2664242f61e165e301ae36
Nicolas Heredia <nicofheredia@gmail.com>
2016-03-04 20:34:54 -0300

    setting staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.assets.initialize_on_precompile = false
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -27,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
