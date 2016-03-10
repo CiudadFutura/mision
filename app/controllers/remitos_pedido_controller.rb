615b2bd59dd65cf7e14d12788fa6ba44cc85d640
Nicolas Heredia <nicofheredia@gmail.com>
2016-03-10 08:43:06 -0300

    Fixed remitos selected

--- app/controllers/remitos_pedido_controller.rb
+++ app/controllers/remitos_pedido_controller.rb
@@ -4,7 +4,7 @@ class RemitosPedidoController < ApplicationController
     if current_usuario.admin?
       @ciclos = Compra.all.order('fecha_fin_compras DESC')
       if(params[:ciclo_id])
-        pedidos = Pedido.where(compra_id: params[:ciclo_id]).group(:circulo_id)
+        pedidos = Pedido.where(compra_id: params[:ciclo_id])
         @reporte = Pedido.remitos(pedidos)
       end
     end
