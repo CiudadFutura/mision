a5ea63933a5a4a7d6a5652cea41f4ff022b00e0c
nfheredia <nicofheredia@gmail.com>
2017-02-14 16:45:36 -0300

    Disable migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   before_filter :init_carrito
   before_filter :categorias_menu
-	before_filter :migrate
+	#before_filter :migrate
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   layout 'layout'
