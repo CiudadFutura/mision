4bf5e3193cb495b03f52fbc9ec6a3623c09b9c3f
nfheredia <nicofheredia@gmail.com>
2017-09-06 22:02:31 -0300

    Delete omniauth sign_in process

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,9 +2,7 @@ class Usuario < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :confirmable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:twitter, :facebook]
-
+         :recoverable, :rememberable, :trackable, :validatable
 
   belongs_to :circulo
   has_many :pedidos
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], { scope: 'email,public_profile,user_likes' }
-  provider :facebook, '1885733625009073', '022336ea29f62fb77bbe1bc2e93b4977', { scope: 'email,public_profile,user_likes' }
+  #provider :facebook, '1885733625009073', '022336ea29f62fb77bbe1bc2e93b4977', { scope: 'email,public_profile,user_likes' }
   #provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   #provider :twitter, '1zBu1IhQ6GdPe6192FmNmfpbO', 'lcgFeKuUObhOm1bSsdF3SNkPB9ApQq9i4jVuKhqQhOfOg77Txf'
   #provider :instagram, ENV['INSTAGRAM_KEY'], ENV['INSTAGRAM_SECRET'], scope: 'basic+media+public_content+follower_list+comments+relationships+likes'
