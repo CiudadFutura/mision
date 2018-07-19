f4cf7804fa536ab3db23a8f5a36d035b97741a9a
nfheredia <nicofheredia@gmail.com>
2018-07-19 11:18:59 -0300

    changes in production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,13 @@ Rails.application.configure do
   # SMTP settings for gmail
 
   config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
+      address: 'smtp.gmail.com',
       port: 587,
-      authentication: "plain",
+      domain:'gmail.com',
+      user_name: ENV['GMAIL_USERNAME'],
+      password: ENV['GMAIL_PASSWORD'],
+      authentication: :plain,
       enable_starttls_auto: true,
-      user_name: ENV["GMAIL_USERNAME"],
-      password: ENV["GMAIL_PASSWORD"]
   }
 
 end
