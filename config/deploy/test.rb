0a7a43ad17a81dfe8a25150101790c93f79c7ed4
nfheredia <nicofheredia@gmail.com>
2018-09-17 20:35:43 -0300

    Added js files

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -1,4 +1,4 @@
 set :stage, :test
-set :branch, 'mision_revolucion'
+set :branch, 'septemberLayout'
 
-server '104.131.8.126', user: 'deploy', roles: %w{web app db}
+server '159.203.95.5', user: 'deploy', roles: %w{web app db}
