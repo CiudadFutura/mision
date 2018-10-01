4aa8a5a38603ec6d4f023dd04dbc5ce43be2cd92
nfheredia <nicofheredia@gmail.com>
2018-09-30 22:03:45 -0300

    Footer delete google plus and add Distrito 6 picture

--- app/helpers/compras_helper.rb
+++ app/helpers/compras_helper.rb
@@ -10,7 +10,7 @@ module ComprasHelper
     if text.include?('Distritos 2 y 5')
       img = 'v2/norte.png'
     end
-    if text.include?('Distritos 6')
+    if text.include?('Distrito 6')
       img = 'v2/centro.png'
     end
     return img
