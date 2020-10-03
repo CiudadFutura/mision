7629f7c64cf069d057eb718051582cc01555a072
Nico Heredia <nicofheredia@gmail.com>
2020-10-03 10:25:57 -0300

    issue-74 - logout issue adding libraries

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,9 @@ import 'bootstrap'
 import '../stylesheets/application.scss'
 import '@fortawesome/fontawesome-free/js/all'
 
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
 require('jquery-validation')
 
 
