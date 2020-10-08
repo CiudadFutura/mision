4eb278aa70b9852e50af93c3e0572ffbd24d3784
azacchino <azacchino@users.noreply.github.com>
2020-10-08 20:04:52 -0300

    chage database dafaults to env vars

--- Docker/docker-entrypoint.sh
+++ Docker/docker-entrypoint.sh
@@ -35,43 +35,43 @@ _main() {
         export RAILS_ENV='production'
     fi
 
-    if [ "$RAILS_ENV" = 'production' ]; then
-        if [ ! -z "${DB_NAME}" ]; then
-            sed -i "s/database: my_db_name/database: ${DB_NAME}/g" config/database.yml
-            mai_note DB_NAME "set"
-        else
-            mai_note DB_NAME default
-        fi
+    # if [ "$RAILS_ENV" = 'production' ]; then
+    #     if [ ! -z "${DB_NAME}" ]; then
+    #         sed -i "s/database: my_db_name/database: ${DB_NAME}/g" config/database.yml
+    #         mai_note DB_NAME "set"
+    #     else
+    #         mai_note DB_NAME default
+    #     fi
 
-        if [ ! -z "${DB_USER}" ]; then
-            sed -i "s/username: root/username: ${DB_USER}/g" config/database.yml
-            mai_note DB_USER "set"
-        else
-            mai_note DB_USER default
-        fi
+    #     if [ ! -z "${DB_USER}" ]; then
+    #         sed -i "s/username: root/username: ${DB_USER}/g" config/database.yml
+    #         mai_note DB_USER "set"
+    #     else
+    #         mai_note DB_USER default
+    #     fi
 
-        if [ ! -z "${DB_PASS}" ]; then
-            sed -i "s/password: my_password/password: ${DB_PASS}/g" config/database.yml
-            mai_note DB_PASS "set"
-        else
-            mai_note DB_PASS default
-        fi
+    #     if [ ! -z "${DB_PASS}" ]; then
+    #         sed -i "s/password: my_password/password: ${DB_PASS}/g" config/database.yml
+    #         mai_note DB_PASS "set"
+    #     else
+    #         mai_note DB_PASS default
+    #     fi
 
-        if [ ! -z "${DB_HOST}" ]; then
-            sed -i "s/host: 127.0.0.1/host: ${DB_HOST}/g" config/database.yml
-            mai_note DB_HOST "set"
-        else
-            mai_note DB_HOST default
-        fi
+    #     if [ ! -z "${DB_HOST}" ]; then
+    #         sed -i "s/host: 127.0.0.1/host: ${DB_HOST}/g" config/database.yml
+    #         mai_note DB_HOST "set"
+    #     else
+    #         mai_note DB_HOST default
+    #     fi
 
-        if [ ! -z "${DB_PORT}" ]; then
-            sed -i "s/port: 3306/port: ${DB_PORT}/g" config/database.yml
-            mai_note DB_PORT "set"
-        else
-            mai_note DB_PORT default
-        fi
-        sed -i "s/socket: \/var\/run\/mysqld\/mysqld.sock/#socket: \/var\/run\/mysqld\/mysqld.sock/g" config/database.yml
-    fi
+    #     if [ ! -z "${DB_PORT}" ]; then
+    #         sed -i "s/port: 3306/port: ${DB_PORT}/g" config/database.yml
+    #         mai_note DB_PORT "set"
+    #     else
+    #         mai_note DB_PORT default
+    #     fi
+    #     sed -i "s/socket: \/var\/run\/mysqld\/mysqld.sock/#socket: \/var\/run\/mysqld\/mysqld.sock/g" config/database.yml
+    # fi
 
     # Wait for DB
     tries=50
