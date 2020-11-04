778e830084310dd254e4f819f35c7ab681da78a1
Nico Heredia <nicofheredia@gmail.com>
2020-11-04 10:31:45 -0300

    issue-107: Remove rails cache in menu function

--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -7,4 +7,24 @@ class Categoria < ActiveRecord::Base
   has_and_belongs_to_many :productos
 
 	TODAS = 'Todas'
+
+  def self.build_menu
+    menu = []
+    Categoria.where(parent_id: 0).each do |cat_parent|
+      cat = {
+          id: cat_parent.id,
+          nombre: cat_parent.nombre,
+          subcategorias: []
+      }
+      cat_parent.subcategorias.each do |subcategoria|
+        cat[:subcategorias] << {
+            id: subcategoria.id,
+            nombre: subcategoria.nombre,
+            parent_id: subcategoria.parent_id
+        }
+      end
+      menu << cat
+    end
+    menu
+  end
 end
