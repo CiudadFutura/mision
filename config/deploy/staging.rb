c08bf38ae990cf4f237c19451608f92fe67076af
egasemi <emiliano.arregui@gmail.com>
2022-05-25 20:00:38 -0300

    set port in staging deploy config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :stage, :staging
 set :branch, 'staging'
 
-server '206.189.200.124', user: 'deploy', roles: %w{web app db}
+server '206.189.200.124', user: 'deploy', roles: %w{web app db}, port:12022
